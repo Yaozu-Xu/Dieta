39ac567eeb4dfe8f6840db3b0d66ed7cddfc7ad0
Yaozu-Xu <2589231996@qq.com>
2020-11-15 16:05:40 +0000

    Feature: Create constants file for reuse styles

---
+++ lib/src/assets/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+TextStyle labelStyle = TextStyle(
+    color: Colors.grey[300].withOpacity(0.4), fontWeight: FontWeight.bold);
+TextStyle valueStyle = TextStyle(
+    fontSize: 18, fontWeight: FontWeight.bold, color: Colors.grey[300]);
+TextStyle mealLabelStyle = TextStyle(
+  color: Colors.grey[300],
+  fontSize: 15,
+  letterSpacing: 1.2,
+);
