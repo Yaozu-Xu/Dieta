339e02d75591b841d3dbd151b947e0c8f0d7a580
Yaozu-Xu <2589231996@qq.com>
2020-11-15 16:06:08 +0000

    Feature: Use ListView to layout widgets

--- lib/src/screens/record_screen.dart
+++ lib/src/screens/record_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../widgets/cards/calories_card.dart';
+import '../widgets/cards/weight_info_card.dart';
+import '../widgets/cards/food_card.dart';
 import '../widgets/buttons/bottom_buttons.dart';
 
 class RecordScreen extends StatelessWidget {
@@ -7,16 +9,36 @@ class RecordScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Record', style: TextStyle(letterSpacing: 1.2, color: Colors.grey[200].withOpacity(0.4))),
-        backgroundColor: Theme.of(context).secondaryHeaderColor,
-        automaticallyImplyLeading: false,
-      ),
-      bottomNavigationBar: BottomButtons(0),
-      backgroundColor: Theme.of(context).primaryColorDark,
-      body: (
-        CaloriesCard()
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Record',
+              style: TextStyle(
+                  letterSpacing: 1.2,
+                  color: Colors.grey[200].withOpacity(0.4))),
+          backgroundColor: Theme.of(context).secondaryHeaderColor,
+          automaticallyImplyLeading: false,
+        ),
+        bottomNavigationBar: BottomButtons(0),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Theme.of(context).buttonColor,
+          child: Icon(Icons.add),
+        ),
+        backgroundColor: Theme.of(context).primaryColorDark,
+        body: ListView(
+          children: [
+            Column(children: [CaloriesCard(), WeightInfoCard()]),
+            Row(
+              children: [
+                FoodCard(0, '200', '300'),
+                FoodCard(1, '200', '300'),
+              ],
+            ),
+            Row(
+              children: [
+                FoodCard(2, '200', '300'),
+                FoodCard(3, '200', '300'),
+              ],
+            )
+          ],
+        ));
   }
 }
