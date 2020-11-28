250a6a98cf9c6d2aa2ed17426c0066894d0cccba
Yaozu-Xu <2589231996@qq.com>
2020-11-28 16:11:48 +0000

    Feature: Create food requester to get api data

---
+++ lib/src/requests/food_request.dart
@@ -0,0 +1,26 @@
+import 'package:http/http.dart' as http;
+import 'dart:developer' as developer;
+import '../model/Food.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:convert';
+
+Future<FoodResponse> fetchFood(String query) async {
+  const fields =
+      '{"fields": [ "item_name", "item_type", "item_description", "item_id","nf_calories","nf_protein","nf_sugars", "nf_total_fat"]}';
+
+  final response =  await http.post(
+    'https://api.nutritionix.com/v1_1/search',
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, dynamic>{
+      'appId': DotEnv().env['APPID'],
+      'appKey': DotEnv().env['APPKEY'],
+      'query': query,
+      'fields': jsonDecode(fields)['fields']
+    }),
+  );
+  developer.log('status code' + response.statusCode.toString());
+  developer.log(response.body.toString());
+  return FoodResponse.fromJson(jsonDecode(response.body));
+}
