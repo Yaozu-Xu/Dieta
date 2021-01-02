e43fec8f47376b75bf285a38f191749eb03ebcbb
Yaozu-Xu <2589231996@qq.com>
2021-01-02 22:02:32 +0000

    Refactor: Remove login_form widget and  use its inner content in login screen

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:fyp_dieta/src/widgets/inputs/login_form.dart';
+import 'package:fyp_dieta/src/widgets/buttons/signin_buttons.dart';
+import 'package:fyp_dieta/src/widgets/layouts/login_form_decration.dart';
 
 class LoginScreen extends StatelessWidget {
   static const routeName = '/login';
@@ -9,9 +10,62 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
+        automaticallyImplyLeading: false,
       ),
       body: Column(children: [
-        LoginForm()
+        Container(
+          margin: EdgeInsets.only(top: 30),
+          child: Form(
+            child: Column(children: [
+              LoginInputDecration(
+                placeHolder: 'Email',
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              LoginInputDecration(
+                placeHolder: 'Password',
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 30, right: 20),
+                alignment: Alignment.centerRight,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, "/signup");
+                  },
+                  child: Text(
+                    'Create an account here',
+                    style: TextStyle(
+                      color: Color(0xFF4285F4),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 30),
+                  width: MediaQuery.of(context).size.width,
+                  child: OutlineButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Login',
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+                  )),
+              GoogleSignButtonPrimary(
+                margin: EdgeInsets.only(top: 40),
+              )
+            ]),
+          ),
+        )
       ]),
     );
   }
