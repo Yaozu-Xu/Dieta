8e8ccfa6fcf2e14275452e99c9ecd64823028536
Yaozu-Xu <2589231996@qq.com>
2021-01-24 10:43:49 +0000

    Feature: Add FutureBuilder to await calories settings

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:fyp_dieta/src/redux/states/app_state.dart';
+import 'package:fyp_dieta/src/redux/states/user_state.dart';
+import 'package:fyp_dieta/src/utils/firebase/firestore/UserCollection.dart';
 import 'package:fyp_dieta/src/widgets/cards/calories_card.dart';
 import 'package:fyp_dieta/src/widgets/cards/weight_info_card.dart';
 import 'package:fyp_dieta/src/widgets/cards/food_card.dart';
@@ -6,6 +10,32 @@ import 'package:fyp_dieta/src/widgets/buttons/bottom_buttons.dart';
 
 class HomeScreen extends StatelessWidget {
   static const routeName = '/';
+
+  int suggestCalories({int index, int totalCalories}) {
+    switch (index) {
+      case 0:
+        {
+          return (totalCalories / 4 - 100).round();
+        }
+        break;
+      case 1:
+        {
+          return (totalCalories / 4 + 100).round();
+        }
+        break;
+      case 2:
+        {
+          return (totalCalories / 4 + 100).round();
+        }
+        break;
+      case 3:
+        {
+          return (totalCalories / 4 - 100).round();
+        }
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,22 +56,77 @@ class HomeScreen extends StatelessWidget {
           },
         ),
         backgroundColor: Theme.of(context).primaryColorDark,
-        body: ListView(
-          children: [
-            Column(children: [CaloriesCard(), WeightInfoCard()]),
-            Row(
-              children: [
-                FoodCard(0, '200', '300'),
-                FoodCard(1, '200', '300'),
-              ],
-            ),
-            Row(
-              children: [
-                FoodCard(2, '200', '300'),
-                FoodCard(3, '200', '300'),
-              ],
-            )
-          ],
-        ));
+        body: StoreConnector<AppState, UserState>(
+            converter: (store) => store.state.userState,
+            builder: (context, userState) {
+              final uid = userState.user.uid;
+              return FutureBuilder(
+                  future: UserCollection().getUserSettings(uid: uid),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return Text("Something went wrong");
+                    }
+                    if (snapshot.connectionState == ConnectionState.done && snapshot.data.data() != null) {
+                      Map<String, dynamic> data = snapshot.data.data();
+                      return ListView(
+                        children: [
+                          Column(children: [
+                            CaloriesCard(totalCalories: data['totalCalories']),
+                            WeightInfoCard(
+                              username: userState.user.displayName,
+                              uid: uid,
+                              weight: data['weight'],
+                              weightStaging: data['weightStaging'],
+                            )
+                          ]),
+                          Row(
+                            children: [
+                              FoodCard(
+                                labelIndex: 0,
+                                intakeCalories: 0,
+                                suggestCaloires: suggestCalories(
+                                  index: 0,
+                                  totalCalories: data['totalCalories'],
+                                ),
+                              ),
+                               FoodCard(
+                                labelIndex: 1,
+                                intakeCalories: 0,
+                                suggestCaloires: suggestCalories(
+                                  index: 1,
+                                  totalCalories: data['totalCalories'],
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                               FoodCard(
+                                labelIndex: 2,
+                                intakeCalories: 0,
+                                suggestCaloires: suggestCalories(
+                                  index: 2,
+                                  totalCalories: data['totalCalories'],
+                                ),
+                              ),
+                               FoodCard(
+                                labelIndex: 3,
+                                intakeCalories: 0,
+                                suggestCaloires: suggestCalories(
+                                  index: 3,
+                                  totalCalories: data['totalCalories'],
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      );
+                    }
+                    return Scaffold(
+                        body: Center(
+                      child: CircularProgressIndicator(),
+                    ));
+                  });
+            }));
   }
 }
