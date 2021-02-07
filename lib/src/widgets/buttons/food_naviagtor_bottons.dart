b933cd6d9fcd862fe7f4049c576be8a9889962e2
Yaozu-Xu <2589231996@qq.com>
2021-02-07 21:53:53 +0000

    Feature: Add navigator functions

--- lib/src/widgets/buttons/food_naviagtor_bottons.dart
+++ lib/src/widgets/buttons/food_naviagtor_bottons.dart
@@ -1,24 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:fyp_dieta/src/screens/food_screen.dart';
 
 class FoodNavigatorBottons extends StatelessWidget {
-  Widget _buildText(String label) {
-    return (Expanded(
-        child: Text(label,
-            style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey[500]))));
+  final int currentSection;
+  final List mealLabelList = ['Breakfast', 'Lunch', 'Dinner', 'Extra'];
+
+  FoodNavigatorBottons({@required this.currentSection});
+
+  Widget _buildText(BuildContext context, int mealType) {
+    Color textColor = Colors.grey[600];
+    if (mealType == this.currentSection) {
+      textColor = Colors.purple;
+    }
+    return Expanded(
+        child: InkWell(
+            onTap: () {
+              Navigator.pushNamed(context, FoodScreen.routeName,
+                  arguments: FoodScreenArguments(mealType: mealType));
+            },
+            child: Container(
+              child: Text(mealLabelList[mealType],
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16, color: textColor)),
+            )));
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 20, left: 30),
+      margin: EdgeInsets.only(top: 15, left: 40, right: 40, bottom: 5),
       child: Row(
         children: [
-         _buildText('Restaurant'),
-         _buildText('Gorcery'),
-         _buildText('Common'),
+          _buildText(context, 0),
+          _buildText(context, 1),
+          _buildText(context, 2),
+          _buildText(context, 3),
         ],
       ),
     );
