a31ea402176e61992cd19e8cd613dc5b24a98914
Yaozu-Xu <2589231996@qq.com>
2021-02-14 16:06:29 +0000

    Feature: Add CRUD functions for calories

--- lib/src/utils/local_storage.dart
+++ lib/src/utils/local_storage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fyp_dieta/src/screens/collection_screen.dart';
 import 'package:fyp_dieta/src/screens/home_screen.dart';
+import 'package:fyp_dieta/src/utils/firebase/firestore/record_collection.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> initUserStorage(BuildContext context, String uid) async {
@@ -13,4 +14,50 @@ Future<void> initUserStorage(BuildContext context, String uid) async {
   } else {
     Navigator.pushNamed(context, HomeScreen.routeName);
   }
+}
+
+// key: current date time
+Future<int> getStepsByDate(
+    {@required String key, @required int steps, @required String uid}) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  final int initialSteps = prefs.getInt(key);
+  if (initialSteps == null) {
+    prefs.setInt(key, steps);
+    // date is note updated
+    if (prefs.getString('today') != key) {
+      final String keyOfYesterday = prefs.getString('today');
+      // upload yesterday data
+      if (keyOfYesterday != null) {
+        await uploadYesterdayData(key: keyOfYesterday, uid: uid, steps: steps);
+      }
+    }
+    prefs.setString('today', key);
+    return 0;
+  }
+  return steps - initialSteps;
+}
+
+Future<int> getColoriesByDate({@required String key}) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  final int calories = prefs.getInt(key);
+  if (calories == null) {
+    prefs.setInt(key, 0);
+    return 0;
+  }
+  return calories;
+}
+
+Future<void> setColoriesByDate(
+    {@required String key, @required int calories}) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.setInt(key, calories);
+}
+
+Future<void> uploadYesterdayData(
+    {@required String uid, @required String key, @required int steps}) async {
+  await RecordCollection(uid: uid, date: key)
+      .updateCaloriesRecord(<String, dynamic>{
+    'steps': steps,
+    'consume': await getColoriesByDate(key: key)
+  });
 }
