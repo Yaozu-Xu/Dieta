47de6e1c125952369d837e5ee235efea1cfaaba5
Yaozu-Xu <2589231996@qq.com>
2021-02-14 15:53:08 +0000

    Feature: Use real data to replace the mocks

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,15 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:fyp_dieta/src/assets/constants.dart';
 import 'package:fyp_dieta/src/redux/states/app_state.dart';
 import 'package:fyp_dieta/src/redux/states/user_state.dart';
-import 'package:fyp_dieta/src/utils/firebase/firestore/UserCollection.dart';
+import 'package:fyp_dieta/src/screens/food_screen.dart';
+import 'package:fyp_dieta/src/utils/firebase/firestore/record_collection.dart';
+import 'package:fyp_dieta/src/utils/firebase/firestore/user_collection.dart';
 import 'package:fyp_dieta/src/widgets/cards/calories_card.dart';
 import 'package:fyp_dieta/src/widgets/cards/weight_info_card.dart';
 import 'package:fyp_dieta/src/widgets/cards/food_card.dart';
 import 'package:fyp_dieta/src/widgets/buttons/bottom_buttons.dart';
+import 'package:redux/redux.dart';
 
-class HomeScreen extends StatelessWidget {
-  static const routeName = '/';
+class HomeScreen extends StatefulWidget {
+  static const String routeName = '/';
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future<DocumentSnapshot> recordSnapshot;
 
   int suggestCalories({int index, int totalCalories}) {
     switch (index) {
@@ -33,100 +45,189 @@ class HomeScreen extends StatelessWidget {
           return (totalCalories / 4 - 100).round();
         }
         break;
+      default:
+        {
+          return null;
+        }
+        break;
     }
+  }
+
+  int getCaloriesByType({dynamic data, int mealType}) {
+    int calories = 0;
+    if (data.containsKey('food') as bool) {
+      data['food'].forEach((dynamic meal) {
+        if (meal['mealType'] == mealType) {
+          calories += meal['calories'] as int;
+        }
+      });
+    }
+    return calories;
+  }
+
+  Map<String, int> getNutrition({Map<String, dynamic> data}) {
+    final Map<String, int> nutrition = <String, int>{
+      'sugar': 0,
+      'protein': 0,
+      'fat': 0
+    };
+    if (data.containsKey('food')) {
+      data['food'].forEach((dynamic meal) {
+        nutrition['sugar'] += meal['suagrs'] as int;
+        nutrition['protein'] += meal['protein'] as int;
+        nutrition['fat'] += meal['fat'] as int;
+      });
+    }
+    return nutrition;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Record',
-              style: TextStyle(
-                  letterSpacing: 1.2,
-                  color: Colors.grey[200].withOpacity(0.4))),
-          backgroundColor: Theme.of(context).secondaryHeaderColor,
-          automaticallyImplyLeading: false,
-        ),
-        bottomNavigationBar: BottomButtons(0),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Theme.of(context).buttonColor,
-          child: Icon(Icons.add),
-          onPressed: () {
-            Navigator.pushNamed(context, '/food');
-          },
-        ),
-        backgroundColor: Theme.of(context).primaryColorDark,
-        body: StoreConnector<AppState, UserState>(
-            converter: (store) => store.state.userState,
-            builder: (context, userState) {
-              final uid = userState.user.uid;
-              return FutureBuilder(
+    return StoreConnector<AppState, UserState>(
+        converter: (Store<AppState> store) => store.state.userState,
+        builder: (BuildContext context, UserState userState) {
+          final String uid = userState.user.uid;
+          final Future<DocumentSnapshot> recordFuture =
+              RecordCollection(uid: uid, date: currentDate)
+                  .getAllRecordsByDate();
+          return Scaffold(
+              appBar: AppBar(
+                centerTitle: true,
+                title: Text('Record',
+                    style: TextStyle(
+                        letterSpacing: 1.2,
+                        color: Colors.grey[200].withOpacity(0.4))),
+                backgroundColor: Theme.of(context).secondaryHeaderColor,
+                automaticallyImplyLeading: false,
+              ),
+              bottomNavigationBar: const BottomButtons(0),
+              floatingActionButton: FloatingActionButton(
+                backgroundColor: Theme.of(context).buttonColor,
+                onPressed: () {
+                  Navigator.pushNamed(context, FoodScreen.routeName,
+                      arguments: FoodScreenArguments(mealType: 0));
+                },
+                child: const Icon(Icons.add),
+              ),
+              backgroundColor: Theme.of(context).primaryColorDark,
+              body: FutureBuilder<DocumentSnapshot>(
                   future: UserCollection().getUserSettings(uid: uid),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasError) {
-                      return Text("Something went wrong");
+                  builder: (BuildContext context,
+                      AsyncSnapshot<dynamic> userSnapshot) {
+                    if (userSnapshot.hasError) {
+                      return const Text('Something went wrong');
                     }
-                    if (snapshot.connectionState == ConnectionState.done && snapshot.data.data() != null) {
-                      Map<String, dynamic> data = snapshot.data.data();
-                      return ListView(
-                        children: [
-                          Column(children: [
-                            CaloriesCard(totalCalories: data['totalCalories']),
-                            WeightInfoCard(
-                              username: userState.user.displayName,
-                              uid: uid,
-                              weight: data['weight'],
-                              weightStaging: data['weightStaging'],
-                            )
-                          ]),
-                          Row(
-                            children: [
-                              FoodCard(
-                                labelIndex: 0,
-                                intakeCalories: 0,
-                                suggestCaloires: suggestCalories(
-                                  index: 0,
-                                  totalCalories: data['totalCalories'],
+                    if (userSnapshot.connectionState == ConnectionState.done &&
+                        userSnapshot.data.data() != null) {
+                      final Map<String, dynamic> data =
+                          userSnapshot.data.data() as Map<String, dynamic>;
+                      return FutureBuilder<DocumentSnapshot>(
+                          future: recordFuture,
+                          builder: (BuildContext context,
+                              AsyncSnapshot<dynamic> recordSnapshot) {
+                            int sugar = 0;
+                            int protein = 0;
+                            int fat = 0;
+                            int breakfastCalories = 0;
+                            int lunchCalories = 0;
+                            int dinnerCalories = 0;
+                            int extraCalories = 0;
+                            int intakeCalories = 0;
+                            if (recordSnapshot.hasData &&
+                                recordSnapshot.data.data() != null) {
+                              final Map<String, dynamic> recordData =
+                                  recordSnapshot.data.data()
+                                      as Map<String, dynamic>;
+
+                              breakfastCalories = getCaloriesByType(
+                                  data: recordData, mealType: 0);
+                              lunchCalories = getCaloriesByType(
+                                  data: recordData, mealType: 1);
+                              dinnerCalories = getCaloriesByType(
+                                  data: recordData, mealType: 2);
+                              extraCalories = getCaloriesByType(
+                                  data: recordData, mealType: 3);
+                              intakeCalories = breakfastCalories +
+                                  lunchCalories +
+                                  dinnerCalories +
+                                  extraCalories;
+                              final Map<String, int> nutrition =
+                                  getNutrition(data: recordData);
+                              sugar = nutrition['sugar'];
+                              fat = nutrition['fat'];
+                              protein = nutrition['protein'];
+                            } else {}
+                            return ListView(
+                              children: <Widget>[
+                                Column(children: <Widget>[
+                                  CaloriesCard(
+                                      totalCalories:
+                                          data['totalCalories'] as int,
+                                      intake: intakeCalories,
+                                      suagr: sugar,
+                                      protein: protein,
+                                      fat: fat,
+                                      uid: uid),
+                                  WeightInfoCard(
+                                    username: userState.user.displayName,
+                                    uid: uid,
+                                    weight: data['weight'],
+                                    weightStaging: data['weightStaging'] as int,
+                                  )
+                                ]),
+                                Row(
+                                  children: <Widget>[
+                                    FoodCard(
+                                      labelIndex: 0,
+                                      intakeCalories: breakfastCalories,
+                                      suggestCaloires: suggestCalories(
+                                        index: 0,
+                                        totalCalories:
+                                            data['totalCalories'] as int,
+                                      ),
+                                    ),
+                                    FoodCard(
+                                      labelIndex: 1,
+                                      intakeCalories: lunchCalories,
+                                      suggestCaloires: suggestCalories(
+                                        index: 1,
+                                        totalCalories:
+                                            data['totalCalories'] as int,
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ),
-                               FoodCard(
-                                labelIndex: 1,
-                                intakeCalories: 0,
-                                suggestCaloires: suggestCalories(
-                                  index: 1,
-                                  totalCalories: data['totalCalories'],
-                                ),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                               FoodCard(
-                                labelIndex: 2,
-                                intakeCalories: 0,
-                                suggestCaloires: suggestCalories(
-                                  index: 2,
-                                  totalCalories: data['totalCalories'],
-                                ),
-                              ),
-                               FoodCard(
-                                labelIndex: 3,
-                                intakeCalories: 0,
-                                suggestCaloires: suggestCalories(
-                                  index: 3,
-                                  totalCalories: data['totalCalories'],
-                                ),
-                              ),
-                            ],
-                          )
-                        ],
-                      );
+                                Row(
+                                  children: <FoodCard>[
+                                    FoodCard(
+                                      labelIndex: 2,
+                                      intakeCalories: dinnerCalories,
+                                      suggestCaloires: suggestCalories(
+                                        index: 2,
+                                        totalCalories:
+                                            data['totalCalories'] as int,
+                                      ),
+                                    ),
+                                    FoodCard(
+                                      labelIndex: 3,
+                                      intakeCalories: extraCalories,
+                                      suggestCaloires: suggestCalories(
+                                        index: 3,
+                                        totalCalories:
+                                            data['totalCalories'] as int,
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            );
+                          });
                     }
-                    return Scaffold(
+                    return const Scaffold(
                         body: Center(
                       child: CircularProgressIndicator(),
                     ));
-                  });
-            }));
+                  }));
+        });
   }
 }
