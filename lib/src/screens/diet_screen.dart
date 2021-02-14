c801b1db1c78cf1ac11b709c0b912784c0311314
Yaozu-Xu <2589231996@qq.com>
2021-02-14 16:07:40 +0000

    Feature: Use FutureBuilder to load data

--- lib/src/screens/diet_screen.dart
+++ lib/src/screens/diet_screen.dart
@@ -1,17 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fyp_dieta/src/model/diet_model.dart';
+import 'package:fyp_dieta/src/utils/firebase/firestore/diet_collection.dart';
 import 'package:fyp_dieta/src/widgets/buttons/bottom_buttons.dart';
+import 'package:fyp_dieta/src/widgets/cards/diet_card.dart';
 
 class DietScreen extends StatelessWidget {
-  static const routeName = '/diet';
+  static const String routeName = '/diet';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Diet'),
-        automaticallyImplyLeading: false,
-      ),
-      bottomNavigationBar: BottomButtons(1),
-    );
+        appBar: AppBar(
+          title: const Text(
+            'Diet',
+          ),
+          backgroundColor: Theme.of(context).secondaryHeaderColor,
+          centerTitle: true,
+          automaticallyImplyLeading: false,
+        ),
+        backgroundColor: Theme.of(context).primaryColorDark,
+        bottomNavigationBar: const BottomButtons(1),
+        body: FutureBuilder<DocumentSnapshot>(
+          future: DietCollection.getLowCaloriesDiets(),
+          builder: (BuildContext context, AsyncSnapshot<dynamic> dietSnapshot) {
+            if (dietSnapshot.hasData && dietSnapshot.data.data() != null) {
+              final List<dynamic> dietlList =
+                  dietSnapshot.data.data()['diets'] as List<dynamic>;
+              return ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  final DietModel dietModel = DietModel.fromJson(
+                      dietlList[index] as Map<String, dynamic>);
+                  return DietCard(
+                    photoUrl: dietModel.photoUrl,
+                    name: dietModel.name,
+                    link: dietModel.link,
+                    description: dietModel.description,
+                  );
+                },
+                itemCount: dietlList.length,
+              );
+            }
+            return const Scaffold(
+                body: Center(
+              child: CircularProgressIndicator(),
+            ));
+          },
+        ));
   }
 }
