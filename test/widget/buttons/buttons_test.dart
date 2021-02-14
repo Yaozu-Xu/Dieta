aeaea1c8e5854e6634484f19e5e1cefa21462114
Yaozu-Xu <2589231996@qq.com>
2021-02-14 16:09:05 +0000

    Test: Create tests for buttons

---
+++ test/widget/buttons/buttons_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fyp_dieta/src/redux/reducers/app_reducer.dart';
+import 'package:fyp_dieta/src/redux/states/app_state.dart';
+import 'package:fyp_dieta/src/widgets/buttons/bottom_buttons.dart';
+import 'package:fyp_dieta/src/widgets/buttons/signin_buttons.dart';
+import 'package:redux/redux.dart';
+
+final Store<AppState> store =
+    Store<AppState>(appReducer, initialState: AppState.intital());
+
+Widget createMockApp(Widget testedWidget) => MediaQuery(
+    data: const MediaQueryData(),
+    child: StoreProvider<AppState>(
+        store: store, child: MaterialApp(home: testedWidget)));
+
+void main() {
+  group('Button widgets testing', () {
+    testWidgets('Load BottomButtons', (WidgetTester tester) async {
+      const  String iconText = 'Diet';
+      await tester.pumpWidget(createMockApp(const BottomButtons(0)));
+      final Finder navigatorFinder = find.byType(BottomNavigationBar);
+      final Finder iconFinder = find.text(iconText);
+      expect(navigatorFinder, findsOneWidget);
+      expect(iconFinder, findsOneWidget);
+    });
+
+    testWidgets('Load SigninButtons', (WidgetTester tester) async {
+      const String buttonText = 'Sign in with Google';
+      await tester.pumpWidget(createMockApp(const GoogleSignButtonPrimary()));
+      final Finder buttonFinder = find.text(buttonText);
+      expect(buttonFinder, findsOneWidget);
+    });
+  });
+}
