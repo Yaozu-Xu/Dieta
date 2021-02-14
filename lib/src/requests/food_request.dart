f44ebc2e2803859ac8f3f4671f1aa252c69d61bb
Yaozu-Xu <2589231996@qq.com>
2021-02-14 15:54:10 +0000

    Style: Fix linter issues

--- lib/src/requests/food_request.dart
+++ lib/src/requests/food_request.dart
@@ -1,15 +1,14 @@
 import 'dart:convert';
 import 'dart:developer' as developer;
 import 'package:http/http.dart' as http;
-import 'package:fyp_dieta/src/model/Food.dart';
+import 'package:fyp_dieta/src/model/food_model.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-
 Future<FoodResponse> fetchFood(String query) async {
-  const fields =
+  const String fields =
       '{"fields": [ "item_name", "item_type", "item_description", "item_id","nf_calories","nf_protein","nf_sugars", "nf_total_fat"]}';
 
-  final response =  await http.post(
+  final http.Response response = await http.post(
     'https://api.nutritionix.com/v1_1/search',
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
@@ -21,7 +20,8 @@ Future<FoodResponse> fetchFood(String query) async {
       'fields': jsonDecode(fields)['fields']
     }),
   );
-  developer.log('status code' + response.statusCode.toString());
+  developer.log('status code${response.statusCode}');
   developer.log(response.body.toString());
-  return FoodResponse.fromJson(jsonDecode(response.body));
+  return FoodResponse.fromJson(
+      jsonDecode(response.body) as Map<String, dynamic>);
 }
