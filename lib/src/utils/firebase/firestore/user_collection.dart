63c3cf1cfacb32e689d9dc8e2a6ed40582218991
Yaozu-Xu <2589231996@qq.com>
2021-02-14 15:48:02 +0000

    Refactor: Redesign User Collection

--- lib/src/utils/firebase/firestore/user_collection.dart
+++ lib/src/utils/firebase/firestore/user_collection.dart
@@ -1,14 +1,14 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
 class UserCollection {
   CollectionReference user = FirebaseFirestore.instance.collection('User');
 
-  Future addUserSettings(String uid, Map data) async {
-    return await user.doc(uid).set(data);
+  Future<void> addUserSettings(String uid, Map<String, dynamic> data) async {
+    user.doc(uid).set(data);
   }
 
   Future<DocumentSnapshot> getUserSettings({@required String uid}) async {
-    return await user.doc(uid).get();
+    return user.doc(uid).get();
   }
 }
