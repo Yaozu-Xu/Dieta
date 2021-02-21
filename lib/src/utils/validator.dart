da57ae8646f8c01ce01e2947455f490ed3039267
Yaozu-Xu <2589231996@qq.com>
2021-02-21 16:03:30 +0000

    Feature: Add new validators

--- lib/src/utils/validator.dart
+++ lib/src/utils/validator.dart
@@ -1,12 +1,12 @@
 class Validator {
-
-  static final RegExp emailReg = RegExp(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+  static final RegExp emailReg = RegExp(
+      r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
 
   static String emailValidator(String value) {
     if (value.isEmpty) {
       return 'Please enter some text';
     }
-    if(!emailReg.hasMatch(value)) {
+    if (!emailReg.hasMatch(value)) {
       return 'Email is invalid';
     }
     return null;
@@ -16,7 +16,7 @@ class Validator {
     if (value.isEmpty) {
       return 'Please enter some text';
     }
-    if(value.length < 6) {
+    if (value.length < 6) {
       return 'Password is too short';
     }
     return null;
@@ -26,8 +26,51 @@ class Validator {
     if (value.isEmpty) {
       return 'Please enter some text';
     }
-    if(value.length < 3) {
+    if (value.length < 3) {
       return 'Username is too short';
+    }
+    return null;
+  }
+
+  static String heightValidator(String value) {
+    try {
+      if (value.isEmpty) {
+        return 'Please enter some text';
+      }
+      if (double.parse(value) < 1.0 || double.parse(value) > 300) {
+        return 'Invalid height';
+      }
+      return null;
+    } catch (err) {
+      return 'Invalid height';
+    }
+  }
+
+  static String weightValidator(String value) {
+    try {
+      if (value.isEmpty) {
+        return 'Please enter some text';
+      }
+      if (double.parse(value) <= 1.0 || double.parse(value) > 400) {
+        return 'Invalid weight';
+      }
+    } catch (err) {
+      return 'Invalid weight';
+    }
+
+    return null;
+  }
+
+  static String ageValidator(String value) {
+    try {
+      if (value.isEmpty) {
+        return 'Please enter some text';
+      }
+      if (int.parse(value) < 0 || int.parse(value) > 200) {
+        return 'Invalid age';
+      }
+    } catch (err) {
+      return 'Invalid age';
     }
     return null;
   }
