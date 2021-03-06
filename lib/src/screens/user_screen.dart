c82155ebf816aab4c713c1dbcedd81a0cec15918
Yaozu-Xu <2589231996@qq.com>
2021-03-06 14:38:24 +0000

    Feature: Add settings of notifcation

--- lib/src/screens/user_screen.dart
+++ lib/src/screens/user_screen.dart
@@ -7,12 +7,32 @@ import 'package:fyp_dieta/src/widgets/layouts/user_stream_builder.dart';
 import 'package:fyp_dieta/src/widgets/buttons/bottom_buttons.dart';
 import 'package:fyp_dieta/src/assets/constants.dart';
 import 'package:fyp_dieta/src/utils/firebase/sign_in.dart';
+import 'package:fyp_dieta/src/utils/local_storage.dart';
 import 'package:redux/redux.dart';
-
 import 'collection_screen.dart';
 
-class UserScreen extends StatelessWidget {
+class UserScreen extends StatefulWidget {
   static const String routeName = '/user';
+
+  @override
+  _UserScreenState createState() => _UserScreenState();
+}
+
+class _UserScreenState extends State<UserScreen> {
+  bool _hasDietNotifications;
+
+  @override
+  void initState() {
+    super.initState();
+    getDietNotifications().then((bool value) => setState(() {
+          _hasDietNotifications = value;
+        }));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   final List<String> _sportsLevelLabelLists = <String>[
     'Light',
@@ -43,11 +63,56 @@ class UserScreen extends StatelessWidget {
         backgroundImage: AssetImage('lib/src/assets/image/user.png'));
   }
 
+  Future<void> _showNotificationDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+            builder: (BuildContext cotext, Function setState) {
+          return AlertDialog(
+            title: const Text('Notification Settings',
+                textAlign: TextAlign.center),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  Row(children: <Widget>[
+                    const Text('Diet Notification'),
+                    const Spacer(),
+                    Checkbox(
+                      checkColor: Colors.blue,
+                      activeColor: Colors.grey[200],
+                      value: _hasDietNotifications,
+                      onChanged: (bool value) async {
+                        setState(() {
+                          _hasDietNotifications = value;
+                        });
+                        await setDietNotifications(value: value);
+                      },
+                    ),
+                  ])
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Exit'),
+              ),
+            ],
+          );
+        });
+      },
+    );
+  }
+
   Widget _buildMenuList(BuildContext context, UserState userState) {
     final String sportsLabel =
         _sportsLevelLabelLists[userState.settings.sportsLevel];
     final String weightLabel =
-     _weightStagingList[userState.settings.weightStaging];
+        _weightStagingList[userState.settings.weightStaging];
     return ListView(
       children: <Widget>[
         _buildListItem(
@@ -92,6 +157,22 @@ class UserScreen extends StatelessWidget {
             )),
         _buildRowWitBtn(
             context: context,
+            icon: Icons.notifications,
+            child: Container(
+              margin: const EdgeInsets.only(right: 40),
+              child: OutlineButton(
+                  borderSide:
+                      BorderSide(color: Colors.grey[300].withOpacity(0.4)),
+                  onPressed: () async {
+                    await _showNotificationDialog(context);
+                  },
+                  child: Text(
+                    'Notifications',
+                    style: listLabelStyle,
+                  )),
+            )),
+        _buildRowWitBtn(
+            context: context,
             icon: Icons.logout,
             child: Container(
               margin: const EdgeInsets.only(right: 40),
@@ -106,7 +187,7 @@ class UserScreen extends StatelessWidget {
                     'Log Out',
                     style: listLabelStyle,
                   )),
-            )),
+            ))
       ],
     );
   }
