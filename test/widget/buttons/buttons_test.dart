71d2dde71d6bb91f72ba76e24c743b24d16cd96e
Yaozu-Xu <2589231996@qq.com>
2021-03-06 14:45:10 +0000

    Test: Add new test of FloatButton

--- test/widget/buttons/buttons_test.dart
+++ test/widget/buttons/buttons_test.dart
@@ -5,6 +5,7 @@ import 'package:fyp_dieta/src/redux/reducers/app_reducer.dart';
 import 'package:fyp_dieta/src/redux/states/app_state.dart';
 import 'package:fyp_dieta/src/widgets/buttons/bottom_buttons.dart';
 import 'package:fyp_dieta/src/widgets/buttons/signin_buttons.dart';
+import 'package:fyp_dieta/src/widgets/buttons/floating_buttons.dart';
 import 'package:redux/redux.dart';
 
 final Store<AppState> store =
@@ -18,7 +19,7 @@ Widget createMockApp(Widget testedWidget) => MediaQuery(
 void main() {
   group('Button widgets testing', () {
     testWidgets('Load BottomButtons', (WidgetTester tester) async {
-      const  String iconText = 'Diet';
+      const String iconText = 'Diet';
       await tester.pumpWidget(createMockApp(const BottomButtons(0)));
       final Finder navigatorFinder = find.byType(BottomNavigationBar);
       final Finder iconFinder = find.text(iconText);
@@ -31,6 +32,13 @@ void main() {
       await tester.pumpWidget(createMockApp(const GoogleSignButtonPrimary()));
       final Finder buttonFinder = find.text(buttonText);
       expect(buttonFinder, findsOneWidget);
+    });
+
+    testWidgets('Load FloatingButton', (WidgetTester tester) async {
+      await tester
+          .pumpWidget(createMockApp(const MealIconFloatingButton(mealType: 1)));
+      final Finder floatBtnFinder = find.byIcon(Icons.kitchen);
+      expect(floatBtnFinder, findsOneWidget);
     });
   });
 }
