f134cc06fda738981508f507944df679ea3676b0
Yaozu-Xu <2589231996@qq.com>
2021-03-18 08:56:05 +0000

    Fix: Fix the bug of full-screen width

--- lib/src/widgets/common/toast.dart
+++ lib/src/widgets/common/toast.dart
@@ -5,37 +5,33 @@ class Toast {
     final OverlayEntry overlayEntry =
         OverlayEntry(builder: (BuildContext context) {
       return Positioned(
-          top: MediaQuery.of(context).size.height * 0.8,
-          child: Material(
+        top: MediaQuery.of(context).size.height * 0.8,
+        child: Container(
+            color: Colors.transparent,
+            width: MediaQuery.of(context).size.width,
+            alignment: Alignment.center,
             child: Container(
-                color: Colors.transparent,
-                width: MediaQuery.of(context).size.width,
-                alignment: Alignment.center,
-                child: Container(
-                  constraints: const BoxConstraints(maxWidth: 200),
-                  padding: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: const BorderRadius.all(Radius.circular(10)),
-                    boxShadow: <BoxShadow>[
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset:
-                            const Offset(0, 3), // changes position of shadow
-                      ),
-                    ],
+              constraints: const BoxConstraints(maxWidth: 200),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3), // changes position of shadow
                   ),
-                  child: Text(
-                    message,
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: color),
-                  ),
-                )),
-          ));
+                ],
+              ),
+              child: Text(
+                message,
+                style: TextStyle(
+                    fontSize: 16, fontWeight: FontWeight.bold, color: color),
+              ),
+            )),
+      );
     });
     Overlay.of(context).insert(overlayEntry);
     Future<dynamic>.delayed(const Duration(seconds: 2)).then((dynamic value) {
