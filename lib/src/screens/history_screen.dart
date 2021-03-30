f83ce99817c7ebbd1187b244145eb65c3f2d373d
Yaozu-Xu <2589231996@qq.com>
2021-03-30 13:57:33 +0100

    Feature: Display record details monthly

--- lib/src/screens/history_screen.dart
+++ lib/src/screens/history_screen.dart
@@ -29,6 +29,7 @@ class _HistoryScreenState extends State<HistoryScreen>
     with TickerProviderStateMixin {
   AnimationController _animationController;
   CalendarController _calendarController;
+  String _currentMonth;
   final DateFormat _format = DateFormat('yyyy-MM-dd');
 
   @override
@@ -41,10 +42,11 @@ class _HistoryScreenState extends State<HistoryScreen>
       duration: const Duration(milliseconds: 400),
     );
     _animationController.forward();
+    _currentMonth = currentDate.substring(0, 7);
   }
 
   Future<void> setSelectedDay() async {
-     _calendarController.setSelectedDay(_format.parse(await getCurrentDate()));
+    _calendarController.setSelectedDay(_format.parse(await getCurrentDate()));
   }
 
   @override
@@ -55,8 +57,8 @@ class _HistoryScreenState extends State<HistoryScreen>
   }
 
   Future<void> _onDaySelected(DateTime day) async {
-      setCurrentDate(date: _format.format(day));
-      Navigator.pushNamed(context, HomeScreen.routeName);
+    setCurrentDate(date: _format.format(day));
+    Navigator.pushNamed(context, HomeScreen.routeName);
   }
 
   @override
@@ -64,106 +66,152 @@ class _HistoryScreenState extends State<HistoryScreen>
     final HistoryScreenArguments args =
         ModalRoute.of(context).settings.arguments as HistoryScreenArguments;
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Record', style: appBarStyle),
-        backgroundColor: Theme.of(context).secondaryHeaderColor,
-      ),
-      body: Column(
-        children: <Widget>[
-          _buildTableCalendarWithBuilders(uid: args.uid),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Record', style: appBarStyle),
+          backgroundColor: Theme.of(context).secondaryHeaderColor,
+        ),
+        body: FutureBuilder<QuerySnapshot>(
+            future: RecordCollection(uid: args.uid).getAllRecords(),
+            builder:
+                (BuildContext context, AsyncSnapshot<dynamic> recordSnapshot) {
+              final Map<DateTime, List<RecordMap>> events =
+                  <DateTime, List<RecordMap>>{};
+              final Map<String, int> recordsInMonth = <String, int>{
+                'diet': 0,
+                'sports': 0
+              };
+              // load events if have records
+              if (recordSnapshot.hasData) {
+                final List<QueryDocumentSnapshot> recordList = recordSnapshot
+                    .data.documents as List<QueryDocumentSnapshot>;
+                for (final DocumentSnapshot e in recordList) {
+                  events[_format.parse(e.id)] = <RecordMap>[
+                    RecordMap(
+                        hasFoodRecord: e.data().containsKey('food'),
+                        hasStepsRecord: e.data().containsKey('steps'))
+                  ];
+                }
+                // calculate diets and sports in this month
+                events.forEach((DateTime k, List<dynamic> v) {
+                  if ( _currentMonth ==
+                      k.toString().substring(0, 7)) {
+                    final RecordMap m = v.first as RecordMap;
+                    if (m.hasFoodRecord) {
+                      recordsInMonth['diet'] = ++recordsInMonth['diet'];
+                    }
+                    if (m.hasStepsRecord) {
+                      recordsInMonth['sports'] = ++recordsInMonth['sports'];
+                    }
+                  }
+                });
+              }
+              return Column(
+                children: <Widget>[
+                  _buildTableCalendarWithBuilders(events: events),
+                  _buildDietDescriptios(count: recordsInMonth['diet']),
+                  _buildSportsDescriptios(count: recordsInMonth['sports'])
+                ],
+              );
+            }));
   }
 
-  Widget _buildTableCalendarWithBuilders({@required String uid}) {
-    return FutureBuilder<QuerySnapshot>(
-      future: RecordCollection(uid: uid).getAllRecords(),
-      builder: (BuildContext context, AsyncSnapshot<dynamic> recordSnapshot) {
-        final Map<DateTime, List<RecordMap>> events =
-            <DateTime, List<RecordMap>>{};
-        // load events if have records
-        if (recordSnapshot.hasData) {
-          final List<QueryDocumentSnapshot> recordList =
-              recordSnapshot.data.documents as List<QueryDocumentSnapshot>;
-          for (final DocumentSnapshot e in recordList) {
-            events[_format.parse(e.id)] = <RecordMap>[
-              RecordMap(
-                  hasFoodRecord: e.data().containsKey('food'),
-                  hasStepsRecord: e.data().containsKey('steps'))
-            ];
-          }
-        }
-        return TableCalendar(
-          events: events,
-          calendarController: _calendarController,
-          availableCalendarFormats: const<CalendarFormat, String> {
-            CalendarFormat.month: '',
-            CalendarFormat.week: '',
-          },
-          calendarStyle: const CalendarStyle(
-            outsideDaysVisible: false,
-            weekendStyle: TextStyle(color: Colors.black),
-          ),
-          daysOfWeekStyle: DaysOfWeekStyle(
-            weekendStyle: TextStyle(color: Colors.grey[600]),
-          ),
-          headerStyle: const HeaderStyle(
-            centerHeaderTitle: true,
-            formatButtonVisible: false,
-          ),
-          builders: CalendarBuilders(
-            selectedDayBuilder: (BuildContext context, DateTime date, _) {
-              return FadeTransition(
-                opacity: Tween<double>(begin: 0.0, end: 1.0)
-                    .animate(_animationController),
-                child: Container(
-                  margin: const EdgeInsets.all(4.0),
-                  padding: const EdgeInsets.only(top: 14, left: 14),
-                  color: Colors.deepOrange[100],
-                  width: 100,
-                  height: 100,
-                  child: Text(
-                    '${date.day}',
-                    style: const TextStyle().copyWith(fontSize: 16.0),
-                  ),
-                ),
-              );
-            },
-            markersBuilder: (BuildContext context, DateTime date, List<dynamic >events, List<dynamic> holidays) {
-              final List<Widget> children = <Widget>[];
-              if (events.isNotEmpty) {
-                final RecordMap m = events.first as RecordMap;
-                if (m.hasFoodRecord) {
-                  children.add(
-                    Positioned(
-                      right: 1,
-                      bottom: 1,
-                      child: _buildMealMarker()
-                    ),
-                  );
-                }
-                if (m.hasStepsRecord) {
-                  children.add(
-                    Positioned(
-                      left: 1,
-                      top: 1,
-                      child: _buildSportsMarker()
-                    ),
-                  );
-                }
+  Widget _buildDietDescriptios({@required int count}) {
+    return Container(
+        margin: const EdgeInsets.only(left: 40, right: 40, top: 30),
+        child: Row(
+          children: <Widget>[
+          Icon(FlutterIcons.apple_alt_faw5s, color: Colors.red[400], size: 20),
+          const Spacer(),
+          const Text('Have diet records', style: TextStyle(fontWeight: FontWeight.bold)),
+          const Spacer(),
+           Text('$count days')
+        ]));
+  }
 
-              }
-              return children;
-            },
-          ),
-          onDaySelected: (DateTime date, List<dynamic> events, List<dynamic> holidays) {
-            _onDaySelected(date);
-            _animationController.forward(from: 0.0);
-          },
-        );
+  Widget _buildSportsDescriptios({@required int count}) {
+    return Container(
+        margin: const EdgeInsets.only(left: 40, right: 40, top: 30),
+        child: Row(
+          children: <Widget>[
+          Icon(Icons.run_circle_rounded, color: Colors.blue[400], size: 22),
+          const Spacer(),
+          const Text('Have sports records', style: TextStyle(
+            fontWeight: FontWeight.bold,
+          )),
+          const Spacer(),
+           Text('$count days')
+        ]));
+  }
+
+  Widget _buildTableCalendarWithBuilders(
+      {@required Map<DateTime, List<dynamic>> events}) {
+    return TableCalendar(
+      events: events,
+      calendarController: _calendarController,
+      availableCalendarFormats: const <CalendarFormat, String>{
+        CalendarFormat.month: '',
+        CalendarFormat.week: '',
       },
+      calendarStyle: const CalendarStyle(
+        outsideDaysVisible: false,
+        weekendStyle: TextStyle(color: Colors.black),
+      ),
+      daysOfWeekStyle: DaysOfWeekStyle(
+        weekendStyle: TextStyle(color: Colors.grey[600]),
+      ),
+      headerStyle: const HeaderStyle(
+        centerHeaderTitle: true,
+        formatButtonVisible: false,
+      ),
+      builders: CalendarBuilders(
+        selectedDayBuilder: (BuildContext context, DateTime date, _) {
+          return FadeTransition(
+            opacity: Tween<double>(begin: 0.0, end: 1.0)
+                .animate(_animationController),
+            child: Container(
+              margin: const EdgeInsets.all(4.0),
+              padding: const EdgeInsets.only(top: 14, left: 14),
+              color: Colors.deepOrange[100],
+              width: 100,
+              height: 100,
+              child: Text(
+                '${date.day}',
+                style: const TextStyle().copyWith(fontSize: 16.0),
+              ),
+            ),
+          );
+        },
+        markersBuilder: (BuildContext context, DateTime date,
+            List<dynamic> events, List<dynamic> holidays) {
+          final List<Widget> children = <Widget>[];
+          if (events.isNotEmpty) {
+            final RecordMap m = events.first as RecordMap;
+            if (m.hasFoodRecord) {
+              children.add(
+                Positioned(right: 1, bottom: 1, child: _buildMealMarker()),
+              );
+            }
+            if (m.hasStepsRecord) {
+              children.add(
+                Positioned(left: 1, top: 1, child: _buildSportsMarker()),
+              );
+            }
+          }
+          return children;
+        },
+      ),
+      onDaySelected:
+          (DateTime date, List<dynamic> events, List<dynamic> holidays) {
+        _onDaySelected(date);
+        _animationController.forward(from: 0.0);
+      },
+      onVisibleDaysChanged: (DateTime start, DateTime end, CalendarFormat format){
+        // set current month
+        setState(() {
+          _currentMonth = start.toString().substring(0, 7);
+        });
+      }
     );
   }
 
@@ -172,11 +220,8 @@ class _HistoryScreenState extends State<HistoryScreen>
         duration: const Duration(milliseconds: 300),
         width: 16.0,
         height: 16.0,
-        child: Icon(
-         Icons.run_circle_rounded,
-          color: Colors.blue[400],
-          size: 18
-        ));
+        child:
+            Icon(Icons.run_circle_rounded, color: Colors.blue[400], size: 18));
   }
 
   Widget _buildMealMarker() {
@@ -184,10 +229,7 @@ class _HistoryScreenState extends State<HistoryScreen>
         duration: const Duration(milliseconds: 300),
         width: 16.0,
         height: 16.0,
-        child: Icon(
-         FlutterIcons.apple_alt_faw5s,
-          color: Colors.red[400],
-          size: 16
-        ));
+        child: Icon(FlutterIcons.apple_alt_faw5s,
+            color: Colors.red[400], size: 16));
   }
 }
